058f4eb12ca32bfc007604f91a539b7fbe3c7165
Anoshkin A.V <faster13rus@gmail.com>
2021-04-11 18:48:55 +0300

    lesson 7

--- solution.rb
+++ solution.rb
@@ -1,3 +1,17 @@
+require 'wrong/assert'
+include Wrong::Assert
+
+hash1 = { one: 1, two: 2, three: 3 }
 # BEGIN (write your solution here)
-range = (1..3)
+hash1 = hash1.merge :key => "value".to_sym
 # END
+
+assert { hash1[:key] == :value }
+
+hash2 = { one: 1, two: 2, three: 3 }
+# BEGIN (write your solution here)
+hash2.delete(:one)
+# END
+
+assert { !hash2.has_key? :one }
+
