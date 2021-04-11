0778ccc1f14401b556033469b4dd962213bacc90
Anoshkin A.V <faster13rus@gmail.com>
2021-04-11 18:16:52 +0300

    lesson 5

--- solution.rb
+++ solution.rb
@@ -1,5 +1,25 @@
+ar1 = [3, 2, 'a', :sym]
+
 # BEGIN (write your solution here)
-sym = :symbol
+ar1 << 10
 # END
 
-assert { sym == :symbol }
+assert { ar1.last == 10 }
+
+ar2 = [3, 2, 'a', :sym]
+ar3 = [:sym, 2, 'b']
+
+# BEGIN (write your solution here)
+ar4 = ar2 & ar3
+# END
+
+assert { ar4 == [2, :sym] }
+
+ar5 = [3, 2, 'a', :sym, 10, 11]
+
+# BEGIN (write your solution here)
+ar6= ar5[2,2]
+# END
+
+assert { ar6 == ['a', :sym, 10] }
+
