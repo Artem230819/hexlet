7d48879baa8dac95aac018e00821853a30d67f2c
Anoshkin A.V <faster13rus@gmail.com>
2021-04-11 18:22:02 +0300

    lesson 6

--- solution.rb
+++ solution.rb
@@ -1,25 +1,3 @@
-ar1 = [3, 2, 'a', :sym]
-
 # BEGIN (write your solution here)
-ar1 << 10
+range = (1..3)
 # END
-
-assert { ar1.last == 10 }
-
-ar2 = [3, 2, 'a', :sym]
-ar3 = [:sym, 2, 'b']
-
-# BEGIN (write your solution here)
-ar4 = ar2 & ar3
-# END
-
-assert { ar4 == [2, :sym] }
-
-ar5 = [3, 2, 'a', :sym, 10, 11]
-
-# BEGIN (write your solution here)
-ar6= ar5[2,2]
-# END
-
-assert { ar6 == ['a', :sym, 10] }
-
