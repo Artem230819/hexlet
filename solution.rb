d22361680210ced11545558608848e23dfb071e5
Anoshkin A.V <faster13rus@gmail.com>
2021-04-11 19:24:01 +0300

    lesson 9

--- solution.rb
+++ solution.rb
@@ -1,11 +1,30 @@
 require 'wrong/assert'
 include Wrong::Assert
 
-# BEGIN (write your solution here)
-def cube(a)
-  a**3
+def result(a, b, c, d)
+  # BEGIN (write your solution here)
+  if a == c && b == d
+
+    2
+
+  elsif a > b && c > d || a < b && c < d || a == b && c == d
+
+    1
+
+  else
+
+    0
+
+  end
+  # END
 end
-# END
 
-assert { cube(3) == 27 }
-
+assert { 1 == result(1, 2, 1, 3) }
+assert { 1 == result(2, 1, 3, 1) }
+assert { 1 == result(1, 1, 2, 2) }
+assert { 0 == result(2, 2, 1, 2) }
+assert { 0 == result(2, 2, 3, 2) }
+assert { 0 == result(1, 2, 3, 2) }
+assert { 2 == result(0, 0, 0, 0) }
+assert { 2 == result(1, 2, 1, 2) }
+assert { 2 == result(4, 2, 4, 2) }
