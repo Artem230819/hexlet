6ba5af7836742817a96980c4a39768f2df631292
Anoshkin A.V <faster13rus@gmail.com>
2021-04-12 19:29:08 +0300

    lesson 10

--- solution.rb
+++ solution.rb
@@ -1,18 +1,15 @@
-def product(num1, num2, func)
-  # BEGIN (write your solution here)
-  return num1 if num1 == num2
 
-  return func.call(product(num1, num2 - 1, func), num2)
+def my_pow(base, exp)
+  # BEGIN (write your solution here)
+  pow_iter = -> (exp, acc) {
+    return acc if exp == 0
+    pow_iter.call(exp - 1, acc * base)
+  }
   # END
+
+  pow_iter.call(exp, 1)
 end
 
-sum = -> (num1, num2) { return num1 + num2 }
-assert { 1 == product(1, 1, sum) }
-assert { 3 == product(1, 2, sum) }
-
-multi = -> (num1, num2) { return num1 * num2 }
-assert { 6 == product(1, 3, multi) }
-assert { 24 == product(2, 4, multi) }
-
-pow = -> (num1, num2) { return num1 ** num2 }
-assert { 4096 == product(2, 4, pow) }
+assert { my_pow(2, 3) == 8 }
+assert { my_pow(3, 3) == 27 }
+assert { my_pow(1, 2) == 1 }
